1a31e287d0d26c23a8704fff289ce15d0d9bb9d8
iipratte <iipratte@byu.edu>
2026-04-01 22:28:12 -0600

    Fixed category link

--- frontend/src/components/CategoryFilter.tsx
+++ frontend/src/components/CategoryFilter.tsx
@@ -14,7 +14,7 @@ function CategoryFilter({
     const fetchCategories = async () => {
       try {
         const response = await fetch(
-          'https://localhost:5000/api/Water/GetProjectTypes'
+          'https://waterproject-pratte-backend-cyhebjapbuadb0eq.francecentral-01.azurewebsites.net/api/Water/GetProjectTypes'
         );
         const data = await response.json();
         console.log('Fetched categories:', data);
